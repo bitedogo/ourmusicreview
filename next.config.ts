eec85d181677e0ef4db674916d88e7b779016d3b
bitedogo <forsix5020@naver.com>
2026-05-08 15:36:47 +0900

    배포 오류 수정

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,25 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   images: {
-    domains: [
-      "zdggogbgkvgjkjngvxwn.supabase.co",
-      "lh3.googleusercontent.com",
-      "is1-ssl.mzstatic.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "zdggogbgkvgjkjngvxwn.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/profiles/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "is1-ssl.mzstatic.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
