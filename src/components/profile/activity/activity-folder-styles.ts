e96590022ac6eeeb8f4dd34e05d1c16f0b97c10c
bitedogo <forsix5020@naver.com>
2026-07-29 14:51:33 +0900

    마이페이지 나의 활동 정렬 수정

--- src/components/profile/activity/activity-folder-styles.ts
+++ src/components/profile/activity/activity-folder-styles.ts
@@ -5,19 +5,20 @@ import { PROFILE_CONTENT_BAND_CLASS } from "../profile-section-styles";
 export const ACTIVITY_FOLDER_WIDTH = 266;
 export const ACTIVITY_FOLDER_HEIGHT = 282;
 
+/** 모바일도 Figma 풀사이즈 · 1열 / lg 이상 가로 3열 */
 export const ACTIVITY_FOLDER_SHELL_CLASS =
-  "relative block h-[155px] w-[146px] shrink-0 max-[359px]:h-[133px] max-[359px]:w-[125px] lg:h-[282px] lg:w-[266px]";
+  "relative mx-auto block h-[282px] w-[266px] shrink-0";
 
 export const ACTIVITY_FOLDER_SCALE_CLASS =
-  "absolute left-0 top-0 origin-top-left scale-[0.55] max-[359px]:scale-[0.47] lg:scale-100";
+  "absolute left-0 top-0 origin-top-left scale-100";
 
 export const ACTIVITY_PRIVATE_PLACEHOLDER_CLASS =
-  "flex h-[155px] w-[146px] shrink-0 items-center justify-center rounded-[15px] border border-[#D9D9D9] bg-white p-3 max-[359px]:h-[133px] max-[359px]:w-[125px] lg:h-[282px] lg:w-[266px] lg:p-5";
+  "mx-auto flex h-[282px] w-[266px] shrink-0 items-center justify-center rounded-[15px] border border-[#D9D9D9] bg-white p-5";
 
 export const ACTIVITY_COLLECTION_GRID_CLASS =
-  "flex flex-row flex-nowrap items-start justify-center gap-3 lg:flex-wrap lg:gap-[46px]";
+  "flex flex-col items-center gap-6 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-[46px]";
 
-export const ACTIVITY_OWNER_COLLECTION_GRID_CLASS = `mt-8 flex w-full flex-row flex-nowrap items-start justify-center gap-3 lg:absolute lg:top-[117px] lg:mt-0 lg:gap-[46px] lg:p-[5px] ${PROFILE_CONTENT_BAND_CLASS}`;
+export const ACTIVITY_OWNER_COLLECTION_GRID_CLASS = `mt-8 flex w-full flex-col items-center gap-6 lg:absolute lg:top-[117px] lg:mt-0 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-[46px] lg:p-[5px] ${PROFILE_CONTENT_BAND_CLASS}`;
 
 /** Figma 320×3 + gap 13 = 986 · absolute 가운데 */
 export const ACTIVITY_STATS_GRID_CLASS =
